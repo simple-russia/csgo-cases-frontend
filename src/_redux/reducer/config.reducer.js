71d144e9c80c25e08d49ffc62ad90309d23d3b89
Rombulus <alexeyisaev29032001@gmail.com>
2022-02-19 12:00:22 +0300

    added config reducer

---
+++ src/_redux/reducer/config.reducer.js
@@ -0,0 +1,11 @@
+
+const configs = {
+    min_weapons: 3, // the minimum amount of weapons to spin a case
+}
+
+const configReducer = (state = configs, action) => {
+
+    return state;
+}
+
+export default configReducer;
